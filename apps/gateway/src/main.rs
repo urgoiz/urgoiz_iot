bad9dc14e02468067426d57ca4c58286c9a6cf96
urgoiz <aitor.ollo@gmail.com>
2026-04-24 12:11:32 +0200

    [FEAT] Establish monorepo structure with rust workspace and apps

---
+++ apps/gateway/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("--- Garden IoT Gateway ---");
+    println!("Starting the gateway...");
+
+    // Here we will connect to mosquitto
+    println!("Result: Ready to receive data.");
+}
